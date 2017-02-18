a5b7479aed9bd38a75ad4202bfa22bae4cc37c6a
EPICI <rudy2001506@gmail.com>
2017-02-18 17:16:20 -0500

    Add files via upload
    Bezier in O(n) yay

--- src/utils/MathUtils.java
+++ src/utils/MathUtils.java
@@ -1,7 +1,5 @@
 package utils;
 
-import java.util.Arrays;
-
 /**
  * Utility class containing everything math related
  * 
@@ -56,9 +54,9 @@ public final class MathUtils {
 	/**
 	 * Bezier with more points
 	 * <br>
-	 * Very inefficient, uses naive recursive approach
+	 * Uses De Casteljau's to do it in O(n^2) instead of O(2^n)
 	 * <br>
-	 * TODO please someone replace with <a href="https://en.wikipedia.org/wiki/De_Casteljau's_algorithm">De Casteljau's</a>
+	 * https://en.wikipedia.org/wiki/De_Casteljau's_algorithm"
 	 * 
 	 * @param t interpolation value, must be between 0 and 1
 	 * @param ds values to interpolate between
@@ -67,12 +65,80 @@ public final class MathUtils {
 	public static double bezier(double[] ds,double t){
 		int count = ds.length;
 		switch(count){
-		case 0:
-		case 1:throw new IllegalArgumentException("Must have at least two items to interpolate between");
+		case 0:throw new IllegalArgumentException("Must have at least two items to interpolate between");
+		case 1:return ds[0];
 		case 2:return bezier2(ds[0],ds[1],t);
-		default:return bezier2(bezier(Arrays.copyOfRange(ds, 0, count-1),t),bezier(Arrays.copyOfRange(ds, 1, count),t),t);
+		case 3:{
+			double a = ds[0];
+			double b = ds[1];
+			double c = ds[2];
+			double t1 = 1d - t;
+			/*
+			 * Hardcoded for n=3
+			 * Total of 8+1=9 floating point operations
+			 *       1  2  3 4  5  6 7 8
+			 */
+			return (a*t1+2d*b*t)*t1+c*t*t;
+		}
+		case 4:{
+			double a = ds[0];
+			double b = ds[1];
+			double c = ds[2];
+			double d = ds[3];
+			double t1 = 1d - t;
+			/*
+			 * Hardcoded for n=4
+			 * Total of 13+1=9 floating point operations
+			 *       1  2  3 4  5  6  7   8 9 10 11 12 13
+			 */
+			return (a*t1+3d*b*t)*t1*t1+(3d*c*t1+d*t)*t*t;
+		}
+		case 5:{
+			double a = ds[0];
+			double b = ds[1];
+			double c = ds[2];
+			double d = ds[3];
+			double e = ds[4];
+			double t1 = 1d - t;
+			double i = t1*t1;
+			double j = t*t;
+			double k = t1*t;
+			/*
+			 * Hardcoded for n=4
+			 * Total of 14+4=18 floating point operations
+			 *       1  2   3 4  5   6 7   8   9   10 11 12 13 14
+			 */
+			return (a*i + 4d*b*k + 6d*c*j) * i + (4d*d*k + e*j) * j;
+		}
+		default:{
+			double t1 = 1d - t;
+			int n1 = count - 1;
+			int halfn = (n1>>1)+1;
+			double[] choose = chooseDoubleRange(n1,halfn);
+			double[] terms = new double[count];
+			double p = 1d;
+			for(int i=0;i<halfn;i++){
+				terms[i] = ds[i] * choose[i] * p;
+				p *= t;
+			}
+			for(int i=halfn;i<count;i++){
+				terms[i] = ds[i] * choose[n1-i] * p;
+				p *= t;
+			}
+			p = t1;
+			for(int i=1;i<count;i++){
+				terms[n1-i] *= p;
+				p *= t1;
+			}
+			double sum = 0d;
+			for(double v:terms){
+				sum += v;
+			}
+			return sum;
+		}
 		}
 	}
+	
 	/**
 	 * Fast bezier4 for (a,a,b,b)
 	 * <br>
@@ -87,6 +153,42 @@ public final class MathUtils {
 		//Total of 7 floating point operations
 		//      1 2   3 4   5  6  7
 		return t*t*(2d*t-3d)*(a-b)+a;
+	}
+	
+	/**
+	 * Calculates n choose k in O(n) time
+	 * 
+	 * @param n n
+	 * @param k k
+	 * @return n choose k
+	 */
+	public static double chooseDouble(int n,int k){
+		double numerator = 1d;
+		double demoninator = 1d;
+		for(int i=1;i<=k;i++){
+			numerator *= n + 1 - i;
+			demoninator *= i;
+		}
+		return numerator/demoninator;
+	}
+	
+	/**
+	 * Calculates n choose p for 0 &#x2264; p &#x2264; k
+	 * 
+	 * @param n n
+	 * @param k k
+	 * @return values for that range
+	 */
+	public static double[] chooseDoubleRange(int n,int k){
+		int n1 = n+1;
+		double product = 1d;
+		double[] result = new double[k+1];
+		result[0] = 1d;
+		for(int i=1;i<=k;i++){
+			product *= (n1-i)/i;
+			result[i] = product;
+		}
+		return result;
 	}
 
 	/**
